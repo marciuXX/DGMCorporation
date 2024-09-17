5594d0b42cf766461015404ae3e6ba8204403f6b
Michal <marc.michal01@gmail.com>
2024-09-18 01:12:24 +0200

    final website improvement

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
+const allNavLinks = document.querySelectorAll('.nav__link');
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active');
 	navMobile.classList.toggle('nav-mobile--active');
+
+	if (navBtn.classList.contains('is-active')) {
+		document.body.style.setProperty('overflow', 'hidden');
+	} else {
+		document.body.style.removeProperty('overflow');
+	}
+
+	allNavLinks.forEach((item) => {
+		item.addEventListener('click', () => {
+			navBtn.classList.remove('is-active');
+			navMobile.classList.remove('nav-mobile--active');
+			document.body.style.removeProperty('overflow');
+		});
+	});
 };
 
 navBtn.addEventListener('click', handleNav);
